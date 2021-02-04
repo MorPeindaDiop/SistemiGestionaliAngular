da24e4de6df94d44727785e872617ac343ee91bc
MorPeindaDiop <61980976+MorPeindaDiop@users.noreply.github.com>
2021-02-04 12:16:33 +0100

    invoice components

--- src/app/invoice/invoice.module.ts
+++ src/app/invoice/invoice.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { InvoiceRoutingModule } from './invoice-routing.module';
 import { InvoiceComponent } from './invoice.component';
+import { InvoiceMasterComponent } from './invoice-master/invoice-master.component';
+import { InvoiceDetailComponent } from './invoice-detail/invoice-detail.component';
+import { InvoiceSummaryComponent } from './invoice-summary/invoice-summary.component';
 
 
 @NgModule({
-  declarations: [InvoiceComponent],
+  declarations: [InvoiceComponent, InvoiceMasterComponent, InvoiceDetailComponent, InvoiceSummaryComponent],
   imports: [
     CommonModule,
     InvoiceRoutingModule
